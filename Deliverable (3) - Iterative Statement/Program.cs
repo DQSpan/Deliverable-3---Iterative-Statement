9420eb24d30a625545a5afd3e104e21b48c1ea5c
DQSpan <101273121+DQSpan@users.noreply.github.com>
2023-01-29 20:16:57 -0500

    Update Program.cs

--- Deliverable (3) - Iterative Statement/Program.cs
+++ Deliverable (3) - Iterative Statement/Program.cs
@@ -1,46 +1,54 @@
-﻿using System;
-using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-namespace Deliverable (3) - Iterative Statement
-//try-catch block
-try
+namespace IterativeStatement
 {
-    // Ask user to choose for an integer and number series
-    Console.WriteLine("Please enter an integer between 1 and 100: ");
-    // Assign variables
-    int n = Convert.ToInt32(Console.ReadLine());
-
-    if (n > 1 && n < 100)
+    internal class Program
     {
-        Console.WriteLine("Please choose either even or odd: ");
-        string series = Console.ReadLine();
+        static void Main(string[] args)
+        {
+            //try-catch block
+            try
+            {
+                // Ask user to choose for an integer and number series
+                Console.WriteLine("Please enter an integer between 1 and 100: ");
+                // Assign variables
+                int n = Convert.ToInt32(Console.ReadLine());
 
-        //Conditional statement
-        if (series == "odd")
-        {
-            Console.WriteLine("You have selected the odd series. The numbers between 0 and " + n + " are: ");
-            for (int i = 1; i < n; i = i + 2)
-                Console.WriteLine(i);
-        }
-        else if (series == "even")
-        {
-            Console.WriteLine("You have selected the even series. The numbers between 0 and " + n + " are: ");
-            for (int i = 2; i < n; i = i + 2)
-                Console.WriteLine(i);
-        }
-        else
-        {
-            Console.WriteLine("Please enter either odd or even for chosen series.");
-            Console.ReadKey(true);
+                if (n > 1 && n < 100)
+                {
+                    Console.WriteLine("Please choose either even or odd: ");
+                    string series = Console.ReadLine();
+
+                    //Conditional statement
+                    if (series == "odd")
+                    {
+                        Console.WriteLine("You have selected the odd series. The numbers between 0 and " + n + " are: ");
+                        for (int i = 1; i < n; i = i + 2)
+                            Console.WriteLine(i);
+                    }
+                    else if (series == "even")
+                    {
+                        Console.WriteLine("You have selected the even series. The numbers between 0 and " + n + " are: ");
+                        for (int i = 2; i < n; i = i + 2)
+                            Console.WriteLine(i);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter either odd or even for chosen series.");
+                        Console.ReadKey(true);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Your number is not within the range from 1 to 100. Please choose again!");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please only enter a valid integer from 1 to 100.");
+                Console.ReadKey(true);
+            }
         }
     }
-    else
-    {
-        Console.WriteLine("Your number is not within the range from 1 to 100. Please choose again!");
-    }
-}
-catch
-{
-    Console.WriteLine("Please only enter a valid integer from 1 to 100.");
-    Console.ReadKey(true);
 }
